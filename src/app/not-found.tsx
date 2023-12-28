5a57c29afefdc719f7b9c106fb31e61a43c66822
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-28 23:08:18 +0530

    Added Products page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+const NotFoundPage = () => {
+  return (
+    <div role="alert" className="alert">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="stroke-info shrink-0 w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <span className="capitalize">Page Not Found</span>
+      {/* <div>
+          <button className="btn btn-sm">Deny</button>
+          <button className="btn btn-sm btn-primary">Accept</button>
+        </div> */}
+    </div>
+  );
+};
+export default NotFoundPage;
