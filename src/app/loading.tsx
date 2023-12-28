0fdb60ed0cd2dcc5e229621d9839cafe582f78cf
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-28 23:09:40 +0530

    Added Products page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,4 @@
 const LoadingPage = () => {
-  return (
-    <span className="loading loading-dots loading-lg m-auto block"></span>
-  )
-}
-export default LoadingPage
+  return <span className="loading loading-dots loading-lg m-auto block"></span>;
+};
+export default LoadingPage;
