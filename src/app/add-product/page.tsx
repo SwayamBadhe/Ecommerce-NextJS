ffc4a0267459b9b44d53399bd999110666cf281f
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-28 15:43:25 +0530

    First Commit

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,64 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product - Amazon Clone",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw new Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="mb-3 w-full input input-bordered"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        ></textarea>
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="mb-3 w-full input input-bordered"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Proce"
+          type="number"
+          className="mb-3 w-full input input-bordered"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+}
